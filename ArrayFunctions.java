8778f479f08c621e1f6eb026468fe49ced4e8fe7
sumedhaKl <142531637+sumedhaKl@users.noreply.github.com>
2025-11-16 13:10:34 -0800

    Update ArrayFunctions.java

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -8,12 +8,11 @@ public class ArrayFunctions {
         }
         return newArray;
     }
-    public static int[] printEvens() {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        int[] printEvens = new int[nums.length];
+    public static int[] printEvens(int[] inputNums) {
+        int[] printEvens = new int[inputNums.length];
         int evenCount = 0;
 
-        for (int num : nums) {
+        for (int num : inputNums) {
             if (num % 2 == 0) {
                 printEvens[evenCount] = num;
                 evenCount++;
@@ -31,7 +30,8 @@ public class ArrayFunctions {
     public static void main(String[] args) {
     String[] testArr = newArray(4, "test");
     System.out.println(Arrays.toString(testArr));
-    int[] result = printEvens();
+    int[] originalNumsForEvens = {1, 2, 3, 4, 5, 6};
+    int[] result = printEvens(originalNumsForEvens);
     System.out.println(Arrays.toString(result));
     int[] nums = {1, 2, 3};
     int[] resizedNums = resizeArray(nums);
